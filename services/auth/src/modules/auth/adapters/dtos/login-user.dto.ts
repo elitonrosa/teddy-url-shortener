e3d5bb9af46e397745ddc0a35bf01663f109d35f
Eliton Rosa <elitondr@live.com>
2025-05-20 03:25:26 -0300

    refactor(auth): melhora o tratamento de exceções

--- services/auth/src/modules/auth/adapters/dtos/login-user.dto.ts
+++ services/auth/src/modules/auth/adapters/dtos/login-user.dto.ts
@@ -1,9 +1,17 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class LoginUserDto {
+  @ApiProperty({
+    description: 'Email do usuário',
+    example: 'usuario@email.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'Senha do usuário',
+    example: 'senha123',
+  })
   @IsString()
   @MinLength(6)
   password: string;
